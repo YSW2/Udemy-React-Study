be0f65711ad9261e2b577520f6be421668b5076d
YSW2 <jrgyber@gmail.com>
2025-01-19 01:55:07 +0900

    element mapping

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,24 +7,29 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'YSW',
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Jacky',
+        },
+        {
+          name: 'Andrei',
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>hello {this.state.name}</p>
-          <button
-            onClick={() => {
-              this.setState({ name: 'asdf' });
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
